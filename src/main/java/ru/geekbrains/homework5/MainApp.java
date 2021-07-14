3f482331f934852dc4c615c92818476417672c09
OlegZhur <olegodg@yandex.ru>
2021-07-14 10:12:10 +0300

    Вроде бы правильно

--- src/main/java/ru/geekbrains/homework5/MainApp.java
+++ src/main/java/ru/geekbrains/homework5/MainApp.java
@@ -1,18 +1,31 @@
 package ru.geekbrains.homework5;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class MainApp {
     public static final int CARS_COUNT = 4;
+    public static final int HALF_CARS_COUNT = CARS_COUNT/2;
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+        CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT);
         Race race = new Race(new Road(60), new Tunel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier,countDownLatch);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            cyclicBarrier.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            cyclicBarrier.await();
+            cyclicBarrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
--- src/main/java/ru/geekbrains/homework5/Tunel.java
+++ src/main/java/ru/geekbrains/homework5/Tunel.java
@@ -1,6 +1,10 @@
 package ru.geekbrains.homework5;
 
+import java.util.concurrent.Semaphore;
+import static ru.geekbrains.homework5.MainApp.HALF_CARS_COUNT;
+
 public class Tunel extends Stage{
+    Semaphore semaphore = new Semaphore(HALF_CARS_COUNT);
     public Tunel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -10,12 +14,14 @@ public class Tunel extends Stage{
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                semaphore.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                semaphore.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
